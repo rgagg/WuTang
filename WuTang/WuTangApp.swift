5ac6de1a939e615a09ae8d0cda43381558cae16e
Richard Gagg <r.gagg@outlook.com>
2025-02-10 10:07:59 +1100

    Initial Commit

---
+++ WuTang/WuTangApp.swift
@@ -0,0 +1,17 @@
+//
+//  WuTangApp.swift
+//  WuTang
+//
+//  Created by Richard Gagg on 10/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct WuTangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
